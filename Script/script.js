cc622f4c7317c8aed17f267b8dd531914413e958
unknown <hemakumarm72@gmail.com>
2021-06-30 15:24:01 +0530

    max width set in javascript auto

---
+++ Script/script.js
@@ -0,0 +1,41 @@
+var size = function() {
+  function eventHandler() {
+
+    if (screen.width > 675) {
+      const x = (screen.width - 300) + "px";
+  
+      document.getElementsByClassName("container")[0].style.maxWidth = x;
+    } else {
+      const x = (screen.width) + "px";
+  
+      document.getElementsByClassName("container")[0].style.maxWidth = x;
+      }
+  
+  
+  }
+  window.addEventListener('resize', eventHandler, false)
+}
+
+
+
+
+
+
+
+window.document.addEventListener('DOMContentLoaded', size(), false);
+
+// load size
+function myfunction() {
+  if (screen.width > 675) {
+    const x = (screen.width - 300) + "px";
+
+    document.getElementsByClassName("container")[0].style.maxWidth = x;
+  } else {
+    const x = (screen.width) + "px";
+
+    document.getElementsByClassName("container")[0].style.maxWidth = x;
+    }
+
+}
+
+myfunction()
