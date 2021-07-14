86e01726f4ab5cf6728535aa2c6c82031aad3abe
unknown <hemakumarm72@gmail.com>
2021-07-14 16:44:20 +0530

    border color add

--- Script/script.js
+++ Script/script.js
@@ -1,111 +1,111 @@
 var size = function () {
   function eventHandler() {
     if (screen.width > 1000) {
-      const x = screen.width - 300 + "px"
+      const x = screen.width - 300 + 'px';
 
-      document.getElementsByClassName("container")[0].style.maxWidth = x
+      document.getElementsByClassName('container')[0].style.maxWidth = x;
     } else {
-      const x = screen.width + "px"
+      const x = screen.width + 'px';
 
-      document.getElementsByClassName("container")[0].style.maxWidth = x
+      document.getElementsByClassName('container')[0].style.maxWidth = x;
     }
   }
-  window.addEventListener("resize", eventHandler, false)
-}
+  window.addEventListener('resize', eventHandler, false);
+};
 
-window.document.addEventListener("DOMContentLoaded", size(), false)
+window.document.addEventListener('DOMContentLoaded', size(), false);
 function myfunction() {
   if (screen.width > 675) {
-    const x = screen.width - 300 + "px"
+    const x = screen.width - 300 + 'px';
 
-    document.getElementsByClassName("container")[0].style.maxWidth = x
+    document.getElementsByClassName('container')[0].style.maxWidth = x;
   } else {
-    const x = screen.width + "px"
+    const x = screen.width + 'px';
 
-    document.getElementsByClassName("container")[0].style.maxWidth = x
+    document.getElementsByClassName('container')[0].style.maxWidth = x;
   }
 }
 
-document.addEventListener("load", myfunction())
+document.addEventListener('load', myfunction());
 
 // navigation link setup
 
 function btnproduct() {
-  document.getElementById("nav-product").classList.toggle("nav-show")
+  document.getElementById('nav-product').classList.toggle('nav-show');
   // arrow image rototation
   document
-    .getElementById("arrowrotator-product")
-    .classList.toggle("arrow-rotator")
+    .getElementById('arrowrotator-product')
+    .classList.toggle('arrow-rotator');
 }
 
 function btncompany() {
-  document.getElementById("nav-company").classList.toggle("nav-show")
+  document.getElementById('nav-company').classList.toggle('nav-show');
   // arrow image rototation
   document
-    .getElementById("arrowrotator-company")
-    .classList.toggle("arrow-rotator")
+    .getElementById('arrowrotator-company')
+    .classList.toggle('arrow-rotator');
 }
 
 function btnconnect() {
-  document.getElementById("nav-connect").classList.toggle("nav-show")
+  document.getElementById('nav-connect').classList.toggle('nav-show');
 
   // arrow image rototation
   document
-    .getElementById("arrowrotator-connect")
-    .classList.toggle("arrow-rotator")
+    .getElementById('arrowrotator-connect')
+    .classList.toggle('arrow-rotator');
 }
 
 function btnclose() {
-  humberclick.classList.toggle("is-active")
-  navigation_bar.classList.toggle("show")
+  humberclick.classList.toggle('is-active');
+  navigation_bar.classList.toggle('show');
 }
-const linkproduct = document.getElementById("link-product")
-const linkcompany = document.getElementById("link-company")
-const linkconnect = document.getElementById("link-connect")
-const atag = document.getElementsByTagName("A")
-const navigation_bar = document.getElementsByClassName("navigation_bar")[0]
-const product = document.getElementById("nav-product")
-const company = document.getElementById("nav-company")
-const connect = document.getElementById("nav-connect")
-const dropdown = document.getElementsByClassName("dropdown")
-const humberclick = document.getElementById("humburger-1")
+const linkproduct = document.getElementById('link-product');
+const linkcompany = document.getElementById('link-company');
+const linkconnect = document.getElementById('link-connect');
+const atag = document.getElementsByTagName('A');
+const navigation_bar = document.getElementsByClassName('navigation_bar')[0];
+const product = document.getElementById('nav-product');
+const company = document.getElementById('nav-company');
+const connect = document.getElementById('nav-connect');
+const dropdown = document.getElementsByClassName('dropdown');
+const humberclick = document.getElementById('humburger-1');
 
 linkproduct.onclick = function () {
-  btnproduct()
-}
+  btnproduct();
+};
 
 linkcompany.onclick = function () {
-  btncompany()
-}
+  btncompany();
+};
 
 linkconnect.onclick = function () {
-  btnconnect()
-}
+  btnconnect();
+};
 
 humberclick.onclick = function () {
-  btnclose()
-}
+  btnclose();
+};
 
 document.onclick = function (e) {
   if (
-    e.target.className != "link-a" &&
-    e.target.className != "dropdown-link" &&
-    e.target.className != "dropdown"
+    e.target.className != 'link-a' &&
+    e.target.className != 'dropdown-link' &&
+    e.target.className != 'dropdown'
   ) {
-    document.getElementById("nav-product").classList.remove("nav-show")
-    document.getElementById("nav-company").classList.remove("nav-show")
-    document.getElementById("nav-connect").classList.remove("nav-show")
+    document.getElementById('nav-product').classList.remove('nav-show');
+    document.getElementById('nav-company').classList.remove('nav-show');
+    document.getElementById('nav-connect').classList.remove('nav-show');
 
     document
-      .getElementById("arrowrotator-product")
-      .classList.remove("arrow-rotator")
+      .getElementById('arrowrotator-product')
+      .classList.remove('arrow-rotator');
     document
-      .getElementById("arrowrotator-company")
-      .classList.remove("arrow-rotator")
+      .getElementById('arrowrotator-company')
+      .classList.remove('arrow-rotator');
     document
-      .getElementById("arrowrotator-connect")
-      .classList.remove("arrow-rotator")
+      .getElementById('arrowrotator-connect')
+      .classList.remove('arrow-rotator');
   }
-}
+};
 
 /*lazing image load*/
