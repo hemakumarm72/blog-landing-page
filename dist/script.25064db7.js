8ab88c7a5e7fc6dff778e4a452855904722464a0
unknown <hemakumarm72@gmail.com>
2021-08-12 13:41:48 +0530

    phone svg add

--- dist/script.25064db7.js
+++ dist/script.25064db7.js
@@ -252,7 +252,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60281" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58181" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
